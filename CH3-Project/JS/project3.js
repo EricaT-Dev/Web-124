b2c69b78137048953eb2a64bfcc9002003f31909
EricaT-Dev <ericatalley05@gmail.com>
2024-11-02 16:19:08 -0500

    Create project3.js

---
+++ CH3-Project/JS/project3.js
@@ -0,0 +1,34 @@
+/*
+Name : Erica Talley
+Date : 11/2/2024
+ */
+
+// A variable to store your full name
+var myName = "Erica Talley";
+console.log(myName);
+
+// A variable to store your desired annual salary
+var myDesiredSalary = 90000;
+console.log(myDesiredSalary);
+
+// A variable to store your veteran status (hint: either you are a veteran or you are not a veteran)
+var myVetStatus = false;
+console.log(myVetStatus);
+
+//An array to store the names of three of your friends.
+var friendNames;
+friendNames = ["Billy","Haley","Devin"];
+console.log(friendNames);
+
+//An array to store the value of the desired annual salary for your three friends
+var friendSalaries;
+friendSalaries = ["100000","115000","75000"];
+console.log(friendSalaries);
+
+//A literal object to store the first name, last name, and desired annual salary of yet another friend
+friendSalary = {
+    firstName:"Payten",
+    lastName:"Mayfield",
+    desiredSalary: 95000
+};
+console.log(friendSalary);
